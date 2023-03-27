dc5d02dbc9ba64a277635759da57bfb5033beb29
Akingbade Ayotunde <tylerkatsumoto@gmail.com>
2023-03-27 12:10:29 +0100

    Added the light and dark mode functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,26 @@
 // a opening and closing tags changed to Link
 
 import { Link, NavLink } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Logo from '../assets/logo.png';
 
 export const Header = () => {
+  // state for enabling the navbar dropdown menu when size of the screen reduces
   const [hidden, setHidden] = useState(true);
-  // setting vairables for the active and inactive class
+  // state to switch between light and dark mode
+  const [darkMode, setDarkMode] = useState(true);
+
+  // using the useEffect to load the darkmode and light mode action to the browser
+  useEffect(() => {
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+
+    // Adding and removing the class with the darkmode attribute on the web
+    darkMode
+      ? document.documentElement.classList.add('dark')
+      : document.documentElement.classList.remove('dark');
+  }, [darkMode]);
+
+  // setting vairables for the active and inactive class for the navbars selection
   const activeClass =
     'text-base block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white';
   const inactiveClass =
@@ -24,7 +38,7 @@ export const Header = () => {
 
   return (
     <header>
-      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
         <div className="container flex flex-wrap items-center justify-between mx-auto">
           <Link to="/" className="flex items-center">
             <img src={Logo} className="h-6 mr-3 sm:h-9" alt="CineMate Logo" />
@@ -32,7 +46,49 @@ export const Header = () => {
               CineMate
             </span>
           </Link>
-          <div className="flex md:order-2">
+
+          <div id="mobile-nav" className="flex md:order-2">
+            {/* dark mode and light mode button  */}
+            <button
+              // onclick, reverse the state of darkmode, which will be to false, the current state holder for it's useState is true
+              onClick={() => setDarkMode(!darkMode)}
+              data-tooltip-target="navbar-search-example-toggle-dark-mode-tooltip"
+              type="button"
+              data-toggle-dark="light"
+              className="flex items-center p-2 mr-2 text-xs font-medium text-gray-700 bg-white rounded-lg border border-gray-200 toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            >
+              {/* Ternary condition switch for the dark mode state for its icon */}
+              {darkMode ? (
+                <svg
+                  aria-hidden="true"
+                  // sun icon
+                  data-toggle-icon="sun"
+                  className="w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              ) : (
+                <svg
+                  aria-hidden="true"
+                  // oon icon
+                  data-toggle-icon="moon"
+                  className="w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                </svg>
+              )}
+            </button>
+
             <button
               // changing the usetstate's currents holding state to false when clicked
               onClick={() => setHidden(!hidden)}
@@ -108,10 +164,10 @@ export const Header = () => {
             </button>
           </div>
           <div
+            id="nav-links"
             className={`${
               hidden ? 'hidden' : ''
             } items-center justify-between w-full md:flex md:w-auto md:order-1`}
-            id="navbar-search"
           >
             <div className="relative mt-3 md:hidden">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
+  // class to enable the ise of darkmode
+  darkMode: 'class',
   theme: {
     extend: {},
   },
